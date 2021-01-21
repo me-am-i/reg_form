ccff39687cbd08085432824604cc64c5021efce4
user <vsl_sv@bk.ru>
2021-01-21 16:29:35 +0300

    feat: add handlebars

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const host = '127.0.0.1';
 const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('hbs', exphbs({
+  extname: '.hbs',
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  // отправляем ответ
+  res.send('<h2>Привет Express!</h2>');
+});
 
 app.listen(port, host, () => {
   console.log(`Server listens http://${host}:${port}`);
