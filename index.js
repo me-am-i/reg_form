4925e7a2ac5cb469a53a8f59c2e047fffaa29405
user <vsl_sv@bk.ru>
2021-01-22 17:14:10 +0300

    feat: add route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
-
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 
@@ -18,6 +18,16 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/user', [
+  check('login', 'Incorrect login').isLength({ min: 3 }),
+  check('password', 'Too short password').isLength({ min: 5 }),
+], (req, res) => {
+
+  return res.render('index', {
+    success: 'Success',
+  });
 });
 
 app.listen(port, host, () => {
