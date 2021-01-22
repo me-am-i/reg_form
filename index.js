e11acfe0806d79474ff97ad21df1f92e5f5875de
user <vsl_sv@bk.ru>
2021-01-22 16:43:04 +0300

    feat: add bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
+
 
 const app = express();
 
 const host = '127.0.0.1';
 const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 
@@ -12,7 +17,7 @@ app.set('views', './views');
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('index');
 });
 
 app.listen(port, host, () => {
