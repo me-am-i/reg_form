75f66b7ec15755fd7e3f5c88ce64f6b88420ebcd
user <vsl_sv@bk.ru>
2021-01-29 04:08:25 +0300

    refactor: remove unused middleware and templates

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const handlebars = require('express-handlebars');
 const { check, validationResult } = require('express-validator');
 
 const app = express();
@@ -13,21 +12,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
-
-app.set('views', './views');
-app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 app.post('/user', [
   check('login', 'Incorrect login').isLength({ min: 3 }),
   check('password', 'Too short password').isLength({ min: 5 }),
 ], (req, res) => {
-  console.log(req.body.login);
-
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
